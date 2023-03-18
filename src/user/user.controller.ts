da76616e2c52901a42c170f28f808b92197fa327
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2023-03-18 18:43:37 -0400

    feat: get user informations

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { Users } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('')
+  @HttpCode(HttpStatus.OK)
+  getUser(@GetUser() user: Users) {
+    return user;
+  }
+}
